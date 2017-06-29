1ac47bc967f20f4148a2313538ff4d1d1442ed57
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-06-29 16:59:09 +0800

    Download my forks of gcc-ia16 and dosemu2, and use HTTPS rather than FTP to get gnu.org stuff

--- fetch.sh
+++ fetch.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-git clone git@github.com:crtc-demos/gcc-ia16.git
+git clone git@github.com:tkchia/gcc-ia16.git
 git clone git@github.com:crtc-demos/newlib-ia16.git
 git clone git@github.com:crtc-demos/binutils-ia16.git
-git clone -b devel git://git.code.sf.net/p/dosemu/code dosemu
+git clone -b devel git@github.com:tkchia/dosemu2.git dosemu
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2
 tar -xjf gmp-6.1.2.tar.bz2
 wget http://www.mpfr.org/mpfr-current/mpfr-3.1.5.tar.bz2
 tar -xjf mpfr-3.1.5.tar.bz2
-wget ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
+wget https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
 tar -xzf mpc-1.0.3.tar.gz
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.16.1.tar.bz2
+wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.16.1.tar.bz2
 tar -xjf isl-0.16.1.tar.bz2
