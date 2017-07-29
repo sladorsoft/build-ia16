3c11594f4098e180b9fcc6e8ddb3fe1f376f381f
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-07-30 03:01:53 +0800

    Now patches 86sim to "initialize" part of interrupt table and BIOS ROM

--- fetch.sh
+++ fetch.sh
@@ -5,7 +5,9 @@ git clone git@github.com:crtc-demos/binutils-ia16.git
 if git clone git@github.com:reenigne/reenigne.git; then
   patch -dreenigne -p1 <alfe-ubuntu.diff
   patch -dreenigne -p1 <86sim-quiet.diff
-  # ln -s -f reenigne/8088/86sim 86sim
+  patch -dreenigne -p1 <86sim-init.diff
+  rm -f 86sim
+  ln -s reenigne/8088/86sim 86sim
 fi
 git clone -b devel-tk git@github.com:tkchia/dosemu2.git dosemu
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2
