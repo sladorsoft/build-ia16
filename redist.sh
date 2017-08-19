64a05c37642ceea894824babab950d53b6f73c15
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-08-19 13:10:03 +0800

    Added redist.sh, which tries to create a more compact installation tree

---
+++ redist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+cd $(dirname "$0")
+
+rm -rf redist
+mkdir redist
+find prefix -type d \! -path '*/locale' \! -path '*/info' \! -path '*/man' \
+  \! -path '*/plugin/include' \! -path '*/locale/*' \! -path '*/info/*' \
+  \! -path '*/man/*' \! -path '*/plugin/include/*' \! -path '*/ia16-elf/bin' \
+  -print0 | (cd redist && xargs -0 mkdir)
+find prefix \! -type d \! -name '*.info' \! -name '*.[0-9]' \! -name dir \
+  \! -name '*.mo' \! -path '*/plugin/include/*' \! -path '*/ia16-elf/bin/*' | \
+  cpio -p -v redist
+find redist/prefix -executable \! -type d \! -type l \! -name '*.la' \
+  \! -name '*.sh' \! -name 'mk*' -print0 | xargs -0 strip -s -v
