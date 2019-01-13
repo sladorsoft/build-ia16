6caa5a3313616910d608e4c06d962ebeac55bf6d
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2019-01-13 17:12:49 +0800

    fetch.sh: URL for downloading GNU MPFR 3.1.5 broken --- updating

--- fetch.sh
+++ fetch.sh
@@ -13,7 +13,7 @@ if git clone git@github.com:tkchia/reenigne.git || \
 fi
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2
 tar -xjf gmp-6.1.2.tar.bz2
-wget http://www.mpfr.org/mpfr-current/mpfr-3.1.5.tar.bz2
+wget https://www.mpfr.org/mpfr-3.1.5/mpfr-3.1.5.tar.bz2
 tar -xjf mpfr-3.1.5.tar.bz2
 wget https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
 tar -xzf mpc-1.0.3.tar.gz
