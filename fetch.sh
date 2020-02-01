caa2e1894c31c9a9fcd767c0760d6209c7c50d87
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2020-02-01 23:48:48 +0800

    fetch.sh: clarify when falling back on HTTPS for Git downloads
    This fixes a cause for alarm when fetch.sh fails to download
    a Git repository through SSH --- even when it can still do
    so through HTTPS.
    
    See https://github.com/tkchia/build-ia16/issues/1#issuecomment-581038247 .

--- fetch.sh
+++ fetch.sh
@@ -1,16 +1,38 @@
 #!/bin/sh
+
 set -e
-git clone git@github.com:tkchia/gcc-ia16.git || \
-    git clone https://github.com/tkchia/gcc-ia16.git
-git clone git@github.com:tkchia/newlib-ia16.git || \
-    git clone https://github.com/tkchia/newlib-ia16.git
-git clone git@github.com:tkchia/binutils-ia16.git || \
-    git clone https://github.com/tkchia/binutils-ia16.git
-if git clone git@github.com:tkchia/reenigne.git || \
-   git clone https://github.com/tkchia/reenigne.git; then
-  rm -f 86sim
-  ln -s reenigne/8088/86sim 86sim
-fi
+
+banner_beg="`tput bold 2>/dev/null || :`*** "
+banner_end=" ***`tput sgr0 2>/dev/null || :`"
+
+do_banner () {
+    echo "$banner_beg$*$banner_end"
+}
+
+do_git_clone () {
+    name="$1"
+    server="$2"
+    path="$3"
+    do_banner "Trying to download $name Git repository using SSH"
+    if git clone git@"$server":"$path" "$name"; then
+	do_banner "Successfully downloaded $name"
+	return 0
+    fi
+    do_banner "SSH failed; falling back on using HTTPS to download $name"
+    if git clone https://"$server"/"$path" "$name"; then
+	do_banner "Successfully downloaded $name"
+	return 0
+    fi
+    do_banner "Could not download $name!"
+    exit 1
+}
+
+do_git_clone gcc-ia16 github.com tkchia/gcc-ia16.git
+do_git_clone newlib-ia16 github.com tkchia/newlib-ia16.git
+do_git_clone binutils-ia16 github.com tkchia/binutils-ia16.git
+do_git_clone reenigne github.com tkchia/reenigne.git
+rm -f 86sim
+ln -s reenigne/8088/86sim 86sim
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2
 tar -xjf gmp-6.1.2.tar.bz2
 wget https://www.mpfr.org/mpfr-3.1.5/mpfr-3.1.5.tar.bz2
@@ -19,14 +41,10 @@ wget https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
 tar -xzf mpc-1.0.3.tar.gz
 wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.16.1.tar.bz2
 tar -xjf isl-0.16.1.tar.bz2
-git clone git@gitlab.com:tkchia/libi86.git || \
-    git clone https://gitlab.com/tkchia/libi86.git  # GitLab, not GitHub
-git clone git@github.com:tkchia/PDCurses.git pdcurses || \
-    git clone https://github.com/tkchia/PDCurses.git pdcurses
-git clone git@github.com:tkchia/ubasic-ia16.git || \
-    git clone https://github.com/tkchia/ubasic-ia16.git
-git clone git@github.com:tkchia/tinyasm.git || \
-    git clone https://github.com/tkchia/tinyasm.git
+do_git_clone libi86 gitlab.com tkchia/libi86.git  # GitLab, not GitHub
+do_git_clone pdcurses github.com tkchia/PDCurses.git
+do_git_clone ubasic-ia16 github.com tkchia/ubasic-ia16.git
+do_git_clone tinyasm github.com tkchia/tinyasm.git
 wget https://github.com/andrewwutw/build-djgpp/releases/download/v2.8/` \
   `djgpp-linux64-gcc720.tar.bz2
 tar -xjf djgpp-linux64-gcc720.tar.bz2
