ca8f8cdaeb0bbcc6b6e4f4c4655065b9c3a40f32
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2020-09-07 00:22:06 +0800

    GitLab CI: More tweaks & fixes for .gitlab-ci.yml & fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -10,16 +10,22 @@ do_banner () {
 }
 
 do_git_clone () {
+    local name server path opts
     name="$1"
     server="$2"
     path="$3"
+    if test shallow = "$4"; then
+	opts='--depth 1'
+    else
+	opts=
+    fi
     do_banner "Trying to download $name Git repository using SSH"
-    if git clone git@"$server":"$path" "$name"; then
+    if git clone -v $opts git@"$server":"$path" "$name"; then
 	do_banner "Successfully downloaded $name"
 	return 0
     fi
     do_banner "SSH failed; falling back on using HTTPS to download $name"
-    if git clone https://"$server"/"$path" "$name"; then
+    if git clone -v $opts https://"$server"/"$path" "$name"; then
 	do_banner "Successfully downloaded $name"
 	return 0
     fi
@@ -27,10 +33,10 @@ do_git_clone () {
     exit 1
 }
 
-do_git_clone gcc-ia16 "${1-github.com}" tkchia/gcc-ia16.git
-do_git_clone newlib-ia16 "${1-github.com}" tkchia/newlib-ia16.git
-do_git_clone binutils-ia16 "${1-github.com}" tkchia/binutils-ia16.git
-do_git_clone reenigne "${1-github.com}" tkchia/reenigne.git
+do_git_clone gcc-ia16 "${1-github.com}" tkchia/gcc-ia16.git "$2"
+do_git_clone newlib-ia16 "${1-github.com}" tkchia/newlib-ia16.git "$2"
+do_git_clone binutils-ia16 "${1-github.com}" tkchia/binutils-ia16.git "$2"
+do_git_clone reenigne "${1-github.com}" tkchia/reenigne.git "$2"
 rm -f 86sim
 ln -s reenigne/8088/86sim 86sim
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2
@@ -41,10 +47,11 @@ wget https://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
 tar -xzf mpc-1.0.3.tar.gz
 wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.16.1.tar.bz2
 tar -xjf isl-0.16.1.tar.bz2
-do_git_clone libi86 gitlab.com tkchia/libi86.git  # GitLab, not GitHub
-do_git_clone pdcurses "${1-github.com}" tkchia/PDCurses.git
-do_git_clone ubasic-ia16 "${1-github.com}" tkchia/ubasic-ia16.git
-do_git_clone tinyasm "${1-github.com}" tkchia/tinyasm.git
+do_git_clone libi86 "${1-gitlab.com}" tkchia/libi86.git "$2"
+# ^- GitLab, not GitHub!
+do_git_clone pdcurses "${1-github.com}" tkchia/PDCurses.git "$2"
+do_git_clone ubasic-ia16 "${1-github.com}" tkchia/ubasic-ia16.git "$2"
+do_git_clone tinyasm "${1-github.com}" tkchia/tinyasm.git "$2"
 wget https://github.com/andrewwutw/build-djgpp/releases/download/v2.8/` \
   `djgpp-linux64-gcc720.tar.bz2
 tar -xjf djgpp-linux64-gcc720.tar.bz2
